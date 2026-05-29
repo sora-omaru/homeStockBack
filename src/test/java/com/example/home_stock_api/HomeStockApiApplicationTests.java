2f758d73546c66f70fb93010ccda02044b72b1a0
sora-omaru <konggongxia317@gmail.com>
2026-05-29 14:10:52 +0900

    Initial commit

---
+++ src/test/java/com/example/home_stock_api/HomeStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_stock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
