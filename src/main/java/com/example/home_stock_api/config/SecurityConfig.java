0dabb3eb53b397ce6be623bf1058d831958be51a
sora-omaru <konggongxia317@gmail.com>
2026-06-15 13:52:51 +0900

    feat:セキュリティ作成

---
+++ src/main/java/com/example/home_stock_api/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.home_stock_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
