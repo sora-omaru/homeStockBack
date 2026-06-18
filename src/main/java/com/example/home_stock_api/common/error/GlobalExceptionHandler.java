45e7c9f02f3fdd8e9041011bd09ea722c5e25085
sora-omaru <konggongxia317@gmail.com>
2026-06-18 16:29:19 +0900

    feat:validationエラーレスポンス作成

--- src/main/java/com/example/home_stock_api/common/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/home_stock_api/common/error/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.home_stock_api.common.error;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,6 +22,18 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(errorCode.getStatus()).body(response);
     }
 
+@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException exception
+){
+ErrorCode errorCode = ErrorCode.VALIDATION_ERROR;
+ErrorResponse response = new ErrorResponse(
+        errorCode.getStatus().value(),
+        errorCode.name(),
+        errorCode.getMessage()
+        );
+    return ResponseEntity.status(errorCode.getStatus()).body(response);
+}
 
 
 
