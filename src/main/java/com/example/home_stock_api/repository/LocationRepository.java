8ac4366e57bce522ff26ef6700452d479f3f5e41
sora-omaru <konggongxia317@gmail.com>
2026-06-26 10:24:41 +0900

    feat:LocationRepository作成

--- src/main/java/com/example/home_stock_api/repository/LocationRepository.java
+++ src/main/java/com/example/home_stock_api/repository/LocationRepository.java
@@ -1,4 +1,11 @@
 package com.example.home_stock_api.repository;
 
-public class LocationRepository {
+import com.example.home_stock_api.entity.LocationEntity;
+import com.example.home_stock_api.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
+    List<LocationEntity> findByUser(UserEntity user);
 }
