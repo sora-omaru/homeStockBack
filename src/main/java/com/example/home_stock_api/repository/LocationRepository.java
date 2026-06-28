14df16af1f8e38abe8aa8a1121ebd406cea2def5
sora-omaru <konggongxia317@gmail.com>
2026-06-28 17:44:27 +0900

    feat:LocationEntityにfindByIdAndUser_PublicId追加

--- src/main/java/com/example/home_stock_api/repository/LocationRepository.java
+++ src/main/java/com/example/home_stock_api/repository/LocationRepository.java
@@ -5,10 +5,13 @@ import com.example.home_stock_api.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
     //複数帰ってくるためListに返却値を設定している
     List<LocationEntity> findByUser(UserEntity user);
 
     boolean existsByUserAndName(UserEntity user, String name);
-}
+
+    Optional<LocationEntity> findByIdAndUser_PublicId(Long id, UUID publicId);}
