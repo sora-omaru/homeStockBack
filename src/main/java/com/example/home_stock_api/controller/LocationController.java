3a5ecf57f28eefb6139ddf6e16f3295cfe4b4368
sora-omaru <konggongxia317@gmail.com>
2026-06-28 17:43:06 +0900

    feat:LocationControllerにdeleteLocation作成

--- src/main/java/com/example/home_stock_api/controller/LocationController.java
+++ src/main/java/com/example/home_stock_api/controller/LocationController.java
@@ -1,12 +1,12 @@
 package com.example.home_stock_api.controller;
 
 import com.example.home_stock_api.dto.request.LocationCreateRequestDto;
-import com.example.home_stock_api.dto.request.LoginRequestDto;
 import com.example.home_stock_api.dto.response.LocationResponseDto;
 import com.example.home_stock_api.service.LocationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,10 +29,19 @@ public class LocationController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)//201
-    public LocationResponseDto createLocation(@Valid @RequestBody LocationCreateRequestDto request, Authentication authentication){
+    public LocationResponseDto createLocation(@Valid @RequestBody LocationCreateRequestDto request, Authentication authentication) {
         UUID publicId = UUID.fromString(authentication.getName());
 
-        return  locationService.createLocation(publicId,request);
+        return locationService.createLocation(publicId, request);
 
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLocation(Authentication authentication, @PathVariable Long id) {
+        UUID publicId = UUID.fromString(authentication.getName());
+
+        locationService.deleteLocation(publicId, id);
+
+        return ResponseEntity.noContent().build();
     }
 }
