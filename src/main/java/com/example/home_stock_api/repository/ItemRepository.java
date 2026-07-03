b7fb450d5ae2e430ded9816b2a2681511659b070
sora-omaru <konggongxia317@gmail.com>
2026-07-03 22:35:17 +0900

    feat:ItemRepostoryにexistsByUserAndNameとfindByIdAndUserを作成

--- src/main/java/com/example/home_stock_api/repository/ItemRepository.java
+++ src/main/java/com/example/home_stock_api/repository/ItemRepository.java
@@ -1,12 +1,14 @@
 package com.example.home_stock_api.repository;
 
 import com.example.home_stock_api.entity.ItemEntity;
+import com.example.home_stock_api.entity.LocationEntity;
 import com.example.home_stock_api.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
     List<ItemEntity> findByUser(UserEntity user);
@@ -18,4 +20,9 @@ public interface ItemRepository extends JpaRepository<ItemEntity, Long> {
                 WHERE i.user = :user
             """)
     List<ItemEntity> findByUserWithLocation(@Param("user") UserEntity user);
+
+    boolean existsByUserAndName(UserEntity user, String name);
+
+    Optional<LocationEntity> findByIdAndUser(Long id, UserEntity user);
+
 }
