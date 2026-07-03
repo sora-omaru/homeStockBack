6133d0f0940bbb5046a2394ce9b9af1a11a9238a
sora-omaru <konggongxia317@gmail.com>
2026-07-03 22:28:19 +0900

    feat:ItemCreateRequestDtoにバリデーション文言追加

--- src/main/java/com/example/home_stock_api/dto/request/ItemCreateRequestDto.java
+++ src/main/java/com/example/home_stock_api/dto/request/ItemCreateRequestDto.java
@@ -8,31 +8,29 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
-
 public record ItemCreateRequestDto(
+
         @NotBlank(message = "Item名は必須です")
         @Size(max = 100, message = "100文字以内で入力してください")
         String name,
 
-        @NotNull
-        @PositiveOrZero
+        @NotNull(message = "数量は必須です")
+        @PositiveOrZero(message = "数量は0以上で入力してください")
         Integer quantity,
 
-        @NotNull
-        @PositiveOrZero
+        @NotNull(message = "最低在庫数は必須です")
+        @PositiveOrZero(message = "最低在庫数は0以上で入力してください")
         Integer minQuantity,
 
         Long locationId,
 
-        @NotNull
+        @NotNull(message = "カテゴリは必須です")
         ItemCategory category,
 
+        @Size(max = 1000, message = "メモは1000文字以内で入力してください")
         String memo,
 
         LocalDate expirationDate
 
-
 ) {
-
-
 }
