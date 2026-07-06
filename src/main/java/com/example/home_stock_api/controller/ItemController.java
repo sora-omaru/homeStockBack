c62e42a1dd88270b2229fc266c0eecf6e5296964
sora-omaru <konggongxia317@gmail.com>
2026-07-06 14:55:35 +0900

    feat:createItemControllerに作成

--- src/main/java/com/example/home_stock_api/controller/ItemController.java
+++ src/main/java/com/example/home_stock_api/controller/ItemController.java
@@ -1,13 +1,13 @@
 package com.example.home_stock_api.controller;
 
+import com.example.home_stock_api.dto.request.ItemCreateRequestDto;
 import com.example.home_stock_api.dto.response.ItemResponseDto;
 import com.example.home_stock_api.security.provider.CurrentUserProvider;
 import com.example.home_stock_api.service.ItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +21,10 @@ public class ItemController {
     @GetMapping
     public List<ItemResponseDto> getItems(Authentication authentication) {
         return itemService.getItems(currentUserProvider.getPublicId(authentication));
+    }
+
+    @PostMapping
+    public ItemResponseDto createItem(Authentication authentication,@Valid @RequestBody ItemCreateRequestDto request) {
+        return itemService.createItem(currentUserProvider.getPublicId(authentication), request);
     }
 }
