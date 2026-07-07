edd0d50604ef584d8e9e83e7b37a89478058e33c
sora-omaru <konggongxia317@gmail.com>
2026-07-07 13:19:43 +0900

    feat:ItemCOntrollerにDelete作成

--- src/main/java/com/example/home_stock_api/controller/ItemController.java
+++ src/main/java/com/example/home_stock_api/controller/ItemController.java
@@ -6,6 +6,7 @@ import com.example.home_stock_api.security.provider.CurrentUserProvider;
 import com.example.home_stock_api.service.ItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,7 +25,13 @@ public class ItemController {
     }
 
     @PostMapping
-    public ItemResponseDto createItem(Authentication authentication,@Valid @RequestBody ItemCreateRequestDto request) {
+    public ItemResponseDto createItem(Authentication authentication, @Valid @RequestBody ItemCreateRequestDto request) {
         return itemService.createItem(currentUserProvider.getPublicId(authentication), request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItem(Authentication authentication, @PathVariable("id") Long itemId) {
+        itemService.deleteItem(currentUserProvider.getPublicId(authentication), itemId);
+        return ResponseEntity.noContent().build();
     }
 }
