7616df4e9f25264c45673f6b02dd39e80a78f6b1
sora-omaru <konggongxia317@gmail.com>
2026-08-02 11:09:44 +0900

    feat:UpdateItemQuantityRequestDto作成

--- src/main/java/com/example/home_stock_api/dto/request/UpdateItemQuantityRequestDto.java
+++ src/main/java/com/example/home_stock_api/dto/request/UpdateItemQuantityRequestDto.java
@@ -1,4 +1,10 @@
 package com.example.home_stock_api.dto.request;
 
-public class ItemQuantityRequestDto {
-}
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record UpdateItemQuantityRequestDto (
+        @NotNull(message = "数量は必須です")
+        @PositiveOrZero(message = "数量は0以上で入力してください")
+        Integer quantity
+){}
