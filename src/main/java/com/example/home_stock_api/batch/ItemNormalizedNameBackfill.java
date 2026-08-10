5fafe92979cc0440bccc712093fc11d782cbcde5
sora-omaru <konggongxia317@gmail.com>
2026-08-10 11:46:38 +0900

    feat:すでに役割を終えたバッチをコメントアウト

--- src/main/java/com/example/home_stock_api/batch/ItemNormalizedNameBackfill.java
+++ src/main/java/com/example/home_stock_api/batch/ItemNormalizedNameBackfill.java
@@ -1,33 +1,35 @@
-package com.example.home_stock_api.batch;
 
-import com.example.home_stock_api.entity.ItemEntity;
-import com.example.home_stock_api.repository.ItemRepository;
-import com.example.home_stock_api.service.ItemNameNormalizer;
-import jakarta.transaction.Transactional;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-@RequiredArgsConstructor
-// すでに登録してあるItem名を正規化するバッチ処理
-public class ItemNormalizedNameBackfill implements CommandLineRunner {
-
-    private final ItemRepository itemRepository;
-    private final ItemNameNormalizer itemNameNormalizer;
-
-    @Override
-    @Transactional
-    public void run(String... args) {
-        List<ItemEntity> items =
-                itemRepository.findByNormalizedNameIsNull();
-
-        for (ItemEntity item : items) {
-            item.setNormalizedName(
-                    itemNameNormalizer.normalize(item.getName())
-            );
-        }
-    }
-}
+/// /すでに役割を終えたためコメントアウト
+//package com.example.home_stock_api.batch;
+//
+//import com.example.home_stock_api.entity.ItemEntity;
+//import com.example.home_stock_api.repository.ItemRepository;
+//import com.example.home_stock_api.service.ItemNameNormalizer;
+//import jakarta.transaction.Transactional;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Component
+//@RequiredArgsConstructor
+//// すでに登録してあるItem名を正規化するバッチ処理
+//public class ItemNormalizedNameBackfill implements CommandLineRunner {
+//
+//    private final ItemRepository itemRepository;
+//    private final ItemNameNormalizer itemNameNormalizer;
+//
+//    @Override
+//    @Transactional
+//    public void run(String... args) {
+//        List<ItemEntity> items =
+//                itemRepository.findByNormalizedNameIsNull();
+//
+//        for (ItemEntity item : items) {
+//            item.setNormalizedName(
+//                    itemNameNormalizer.normalize(item.getName())
+//            );
+//        }
+//    }
+//}
