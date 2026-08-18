4e124b756f3f52ee2f05e70dd324e1cc367a0670
sora-omaru <konggongxia317@gmail.com>
2026-08-18 11:27:15 +0900

    feat:ItemResponseDtoにPercentage作成

--- src/main/java/com/example/home_stock_api/dto/response/ItemResponseDto.java
+++ src/main/java/com/example/home_stock_api/dto/response/ItemResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.home_stock_api.dto.response;
 
 import com.example.home_stock_api.entity.ItemCategory;
+import com.example.home_stock_api.entity.enums.StockType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,6 +14,9 @@ public class ItemResponseDto {
     private String name;
     private Integer quantity;
     private Integer minQuantity;
+    private StockType stockType;
+    private Integer stockPercentage;
+    private Integer minPercentage;
     private ItemCategory category;
     private Long locationId;
     private String locationName;
