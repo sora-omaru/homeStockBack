87194f048a47e33230436841d3454e5f7b021114
sora-omaru <konggongxia317@gmail.com>
2026-09-01 15:01:46 +0900

    feat:shopping_memoのEntity作成

--- src/main/java/com/example/home_stock_api/entity/ShoppingMemoEntity.java
+++ src/main/java/com/example/home_stock_api/entity/ShoppingMemoEntity.java
@@ -1,4 +1,34 @@
 package com.example.home_stock_api.entity;
 
-public class ShoppingMemo {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "shopping_memo")
+public class ShoppingMemoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = OffsetDateTime.now();
+    }
 }
